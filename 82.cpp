a7f68cab89ab0dc73ded82773753ddb77900ecd0
leymid <124685397+leymid@users.noreply.github.com>
2023-02-27 22:34:57 +0500

    Add files via upload

---
+++ 82.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    for (x=0; x!=123;) {
+        cout << "vvedite chislo: ";
+        cin >> x;
+        cout << "vvedite chislo: ";
+        cin >> x;
+        cout << "vvedite chislo: ";
+        cin >> x;
+        cout << "vvedite chislo: ";
+        cin >> x;
+
+        return 0;
+    }
+    return 0;
+}
